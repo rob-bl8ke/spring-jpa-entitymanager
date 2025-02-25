d0c9b4aee39f554c54e4f3495bf833e5bb256e69
Rob Blake <rob.bl8ke@outlook.com>
2025-02-25 10:02:13 +0200

    Find all students

--- src/main/java/com/luv2code/cruddemo/dao/StudentDAO.java
+++ src/main/java/com/luv2code/cruddemo/dao/StudentDAO.java
@@ -2,7 +2,10 @@ package com.luv2code.cruddemo.dao;
 
 import com.luv2code.cruddemo.entity.Student;
 
+import java.util.List;
+
 public interface StudentDAO {
     void save(Student student);
     Student findById(Integer id);
+    List<Student> findAll();
 }
